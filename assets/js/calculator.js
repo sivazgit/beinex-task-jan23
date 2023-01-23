7f42ffcb42d49cc3c0a1a5e679906817229e8ef0
unknown <sivapriya3256@gmail.com>
2023-01-23 21:50:37 +0530

    calc

---
+++ assets/js/calculator.js
@@ -0,0 +1,30 @@
+// get elemnet value
+let result = document.getElementById("screen-value")
+
+
+// to display number on input text box
+
+function display(num){
+    result.value += num
+}
+
+// to clear content in the text box
+
+function clearBox(){
+    result.value = " ";
+}
+
+// clearing one by one from text box
+
+function removeItem(){
+    crntexp = result.value
+    result.value = crntexp.slice(0,-1)
+}
+
+
+// evaluate expressions (operations)
+
+ function evaluateExp(){
+    result.value = eval(result.value)
+
+ }
